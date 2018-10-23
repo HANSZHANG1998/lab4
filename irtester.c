964c3744e91942afb8064b36a4848cae6e810123
HANSZHANG1998 <hanswilson1234@gmail.com>
2018-10-23 15:03:00 +0000

    “setup”

--- irtester.c
+++ irtester.c
@@ -2,26 +2,36 @@
 #include <wiringPi.h>
 
 int main(int argc, char *argv[])
-{
+{ int a=0;int b=1;
   wiringPiSetup () ;
   pinMode(0, INPUT);
   pinMode(1,OUTPUT);
   pinMode(2,OUTPUT);
-  while(1) {
-    printf("waitting for reset\n");
-    if(digitalRead(0)==1){
-    printf("Got a 1\n");
+
+ 
+while(1){
+
+    if(digitalRead(0) == 1 && digitalRead(0)!=a){
+    printf("Alarm\n");
     digitalWrite(2,HIGH);
     digitalWrite(1,LOW);
     ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/214773840", "Hans", "EECS2031", "NEW MESSAGE");
-}
-   printf("waitting for event\n");
-    if(digitalRead(0) == 0){
-    printf("Got a 0\n");
+    a = digitalRead(0);
+} 
+
+    if(digitalRead(0) == 0 && digitalRead(0)!=b){
+    printf("Waiting for reset\n");
+   printf("Waiting for event\n");
     digitalWrite(2,LOW);
-    digitalWrite(1,HIGH);delay(100);
-    digitalWrite(1,LOW);}
-    printf("Alarm\n");
-  }
-  return 0 ;
+    digitalWrite(1,HIGH);
+    digitalWrite(1,LOW);
+
+} 
+b = digitalRead(0);
+       
+
 }
+  return 0 ;
+
+}
+
