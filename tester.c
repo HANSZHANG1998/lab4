5dd4f58cfc7569cdcbd326064f8d2b595e31bee2
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-10-23 08:25:01 -0400

    Update tester.c

--- tester.c
+++ tester.c
@@ -8,6 +8,6 @@ int main(int argc, char *argv[])
 */
 
   printf("Trying to connect to server\n");
-  ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "Tom", "11111", "mybad 33333");
+  ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/214773840", "Hans", "EECS2031", "NEW MESSAGE");
   return 0;
 }
